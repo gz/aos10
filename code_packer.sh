4d72c62175b692250c65c6913c239a7067da9dc7
Gerd Zellweger <mail@gerdzellweger.com>
2010-09-24 14:47:23 +0000

    scons db excluded in code packer
    git-svn-id: https://svn.vis.ethz.ch/svn/aos10@5 77cbfc24-9232-4b91-9b01-57cbc351378c

--- code_packer.sh
+++ code_packer.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]; then
 	echo "usage: code_packer.sh <milestone_number>"
 else
-	tar cvzf aos-m$1.tar.gz --exclude=.hg --exclude=.svn --exclude=*.pyc --exclude=*.o --exclude=build/* src
+	tar cvzf aos-m$1.tar.gz --exclude=.hg --exclude=.svn --exclude=*.pyc --exclude=*.o --exclude=build/* --exclude=.sconsign.dblite src
 fi
 
 
