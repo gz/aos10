dd484d68fb5304894f36305e5aa6324ccb8fbf99
Gerd Zellweger <mail@gerdzellweger.com>
2010-10-12 22:05:12 +0000

    missing header file
    git-svn-id: https://svn.vis.ethz.ch/svn/aos10@28 77cbfc24-9232-4b91-9b01-57cbc351378c

---
+++ src/sos/frames_test.h
@@ -0,0 +1,11 @@
+#ifndef _FRAMES_TEST_H
+#define _FRAMES_TEST_H
+
+/* Some test functions to ensure functionality of the frame table */
+void frame_test1(void);
+void frame_test2(void);
+void frame_test3(void);
+void frame_test4(void);
+
+
+#endif // _FRAMES_TEST_H
