025d9c88645f8ab11499069a45262c8f3100cb57
Gerd Zellweger <mail@gerdzellweger.com>
2010-11-11 20:06:41 +0000

    benchmarks added
    git-svn-id: https://svn.vis.ethz.ch/svn/aos10@121 77cbfc24-9232-4b91-9b01-57cbc351378c

---
+++ benchmarks/parse.py
@@ -0,0 +1,39 @@
+#
+# Parses the IO Benchmark values and
+# computes mean and Std. Deviation
+#
+from math import sqrt
+
+f = open('5x512.txt')
+
+read = False
+read_values = {}
+write_values = {}
+
+for line in f:
+    if line.find("READ") > -1:
+        read = True
+        print "READ"
+    elif line.find("WRITE") > -1:
+        read = False
+        print "WRITE"
+    elif len(line.split()) == 2:
+        rs, bs = line.split()
+        request_size = int(rs)
+        bandwith = float(bs)
+        if read:
+            if not read_values.has_key(request_size):
+                read_values[request_size] = []
+            read_values[request_size].append(bandwith)        
+        else:
+            if not write_values.has_key(request_size):
+                write_values[request_size] = []
+            write_values[request_size].append(bandwith)
+
+#print read_values
+#print write_values
+    
+for key, values in read_values.items():
+    mean = sum(values) / len(values)
+    deviation = sqrt( sum(map(lambda x: (x-mean)**2, values)) / (len(values)-1) )
+    print key, mean, deviation
