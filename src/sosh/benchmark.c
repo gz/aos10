b59cc8a0882f7496836100a027b83787511a623e
Phil Keller <phkeller@student.ethz.ch>
2010-11-11 19:01:15 +0000

    finished benchmark program, starting to benchmark now
    git-svn-id: https://svn.vis.ethz.ch/svn/aos10@118 77cbfc24-9232-4b91-9b01-57cbc351378c

--- src/sosh/benchmark.c
+++ src/sosh/benchmark.c
@@ -16,13 +16,15 @@ typedef int(*benchmark_function_ptr)(fildes_t, char*, size_t);
 
 static void warmup(benchmark_function_ptr io_function) {
 
-	PRINT_VERBOSE("Warmup phase\n");
+	PRINT_VERBOSE("Warmup phase ");
 
 	fildes_t fd = open(BENCHMARK_FILENAME, O_RDWR);
-	int num_written = 0;
+	int num_processed = 0;
+	int dot_count = 0;
 	for (char* bufptr = buffer; bufptr < buffer+buffer_size; bufptr += BENCHMARK_MAXREQSIZE) {
-		num_written += io_function(fd, bufptr, BENCHMARK_MAXREQSIZE);
-		PRINT_VERBOSE(". ");
+		num_processed += io_function(fd, bufptr, BENCHMARK_MAXREQSIZE);
+		dot_count++;
+		if (dot_count % 16 == 0) PRINT_VERBOSE(". ");
 	}
 	close(fd);
 
@@ -30,21 +32,21 @@ static void warmup(benchmark_function_ptr io_function) {
 }
 
 
-static void print_result(int num_written, unsigned int req_size, uint64_t time_us) {
+static void print_result(unsigned int num_bytes, unsigned int buf_size, unsigned int req_size, uint64_t time_us) {
 
 	// calculate
 	unsigned int time_ms = (unsigned int)(time_us / 1000);
 	unsigned int time_s = (unsigned int)(time_us / 1000000);
 	unsigned int time_ms_part = time_ms - (time_s * 1000);
-	unsigned int speed = (unsigned int)((uint64_t)num_written * 1000000 / time_us);
-	unsigned int speed_part = (unsigned int)((uint64_t)num_written * 1000000000 / time_us) - (speed * 1000);
+	unsigned int speed = (unsigned int)((uint64_t)num_bytes * 1000000 / time_us);
+	unsigned int speed_part = (unsigned int)((uint64_t)num_bytes * 1000000000 / time_us) - (speed * 1000);
 
 	// output
-	PRINT_VERBOSE("Written %d of %d bytes, in chunks of size %d bytes, in %d.%d s.\n", num_written, buffer_size, req_size, time_s, time_ms_part);
+	PRINT_VERBOSE("\nProcessed %d of %d bytes, in chunks of size %d bytes, in %d.%d s.\n", num_bytes, buf_size, req_size, time_s, time_ms_part);
 	time_ms_part--; // hack because of unused warning
 	PRINT_VERBOSE("Exact time in microseconds: %llu us\n", time_us);
-	PRINT_VERBOSE("Writing speed: %d.%d bytes/s\n", speed, speed_part);
-	printf("%d %d.%d\n", req_size, speed, speed_part);
+	PRINT_VERBOSE("Bandwidth: %d.%03d bytes/s\n", speed, speed_part);
+	printf("%d %d.%03d\n", req_size, speed, speed_part);
 
 }
 
@@ -52,48 +54,42 @@ static void measure(benchmark_function_ptr io_function) {
 	// write the file several times while using different request sizes
 	for (unsigned int req_size = BENCHMARK_MINREQSIZE; req_size <= BENCHMARK_MAXREQSIZE; req_size <<= 1) {
 
-		// open file to write, write the whole buffer, close the file
-		fildes_t fd = open(BENCHMARK_FILENAME, FM_WRITE);
-		int num_written = 0;
-		uint64_t time_us = 0ULL;
+		// open file read-write mode
+		fildes_t fd = open(BENCHMARK_FILENAME, O_RDWR);
+		unsigned int num_processed = 0;
 		char* bufptr = buffer;
 
+		// do the measuring
 		uint64_t start = time_stamp();
 		for (int i=0; i < BENCHMARK_CALLS; i++) {
-			num_written += io_function(fd, bufptr, req_size);
+			num_processed += io_function(fd, bufptr, req_size);
 			bufptr += req_size;
 		}
 		uint64_t end = time_stamp();
-		time_us += end-start;
+		uint64_t time_us = end-start;
 
+		// close file and print result
 		close(fd);
-
-		print_result(num_written, req_size, time_us);
+		print_result(num_processed, BENCHMARK_CALLS*req_size, req_size, time_us);
 	}
 }
 
 int benchmark(int argc, char **argv) {
-
+	// allocate a buffer to write files from and read files into
+	buffer_size = BENCHMARK_MAXREQSIZE*BENCHMARK_CALLS;
+	buffer = malloc(buffer_size);
+	memset(buffer,'x',buffer_size);
+	PRINT_VERBOSE("Buffer of size %d bytes created.\n", buffer_size);
+	// repeat benchmark test several times
 	for(int z=0; z<BENCHMARK_REPETITIONS; z++) {
-
-		// Allocate a buffer to write files from and read files into
-		buffer_size = BENCHMARK_MAXREQSIZE*BENCHMARK_CALLS;
-		buffer = malloc(buffer_size);
-		memset(buffer,'x',buffer_size);
-		PRINT_VERBOSE("Buffer of size %d bytes created.\n", buffer_size);
-
-		printf("Benchmarking Write\n");
-		warmup((benchmark_function_ptr) &write);
-		measure((benchmark_function_ptr) &write);
-
-
-		printf("Benchmarking Read\n");
+		printf("\n-- Benchmarking WRITE --\n\n");
+		warmup((benchmark_function_ptr)&write);
+		measure((benchmark_function_ptr)&write);
+		printf("\n-- Benchmarking READ --\n\n");
 		warmup(&read);
 		measure(&read);
-
-		// free buffer
-		free(buffer);
 	}
-
+	// free buffer
+	free(buffer);
 	return 0;
 }
--- src/sosh/benchmark.h
+++ src/sosh/benchmark.h
@@ -3,13 +3,13 @@
 
 /* Benchmark Settings */
 #define BENCHMARK_REPETITIONS	5
-#define BENCHMARK_CALLS   		1024
+#define BENCHMARK_CALLS   		512
 #define BENCHMARK_MAXREQSIZE	(1 << 9)
 #define BENCHMARK_MINREQSIZE	(1 << 4)
 #define BENCHMARK_FILENAME		"benchmark"
 
 /* Benchmark debug print */
-#define BENCHMARK_VERBOSE 1
+//#define BENCHMARK_VERBOSE
 
 #ifdef BENCHMARK_VERBOSE
 	#define PRINT_VERBOSE(x...) printf( x )
